14a1341f16aa2ba3dbede19f60544e156105e331
Breno <brenluz@outlook.com>
2024-04-10 17:43:45 -0300

    limpei o codigo um pouco

--- sketch.js
+++ sketch.js
@@ -1,70 +1,71 @@
-let x;
-let y;
-let xoff;
-let yoff;
-let arr = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p]
-let apple;
+// array with all the lines that make up the mapping of the monitors
+let arr = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p] 
 let initialX = 700
 let initialY = 500
 let xSpeed = 0;
 let ySpeed = 0;
 let breaking = false;
-
+let colors = [];
 function setup() {
     createCanvas(1920,1080);
     reset();
 }
-function reset() {
-    snake = new Snake(initialX,initialY);
-    apple = new Apple(isIn());
-    breaking = false;
+function updateColors() { // makes each body part of the snake have a different color
+    for(let i = 0; i < snake.body.length*3; i++) {
+        // only changes colors that are undefined so the colors don't change every frame
+        if(colors[i] == undefined) { 
+            colors[i] = (random(255)); 
+        }
+    }
+}
+function reset() {  // resets the game
+    snake = new Snake(initialX,initialY); // creates a new snake
+    apple = new Apple(isIn()); // creates a new apple
+    updateColors(); // makes each body part of the snake have a different color
+    breaking = false; // resets the breaking variable (important so the kill function can be called again)
 
 }
 function draw() {
-    x = mouseX;
-    y = mouseY;
     background(0);
-    mapMonitor();
-    apple.show();
-    for(let i = 0; i < arr.length; i++) {
+    mapMonitor(); // draws the map monitor
+    apple.show(); // shows the apple
+    for(let i = 0; i < arr.length; i++) { // checks if the snake has crossed a border
         if(breaking == true) {
             break
         }
         if (crossedLine(snake.x,snake.y,snake.body[4].x,snake.body[4].y,arr[i])) {
             console.log("Crossed line");
-            snake.kill();
-            breaking = true;
+            snake.kill(); // if the snake has crossed a border it dies
+            breaking = true; // breaks the loop
         }
     }
-    if(snake.alive == true) {
+    if(snake.alive == true) { // if the snake is alive it appears on the screen, moves and updates its parameters
     snake.show();
     snake.update();
     snake.step();
     snake.eat();
     }
-    xoff = x;
-    yoff = y;
-
 }
-function keyPressed() {
-    if(keyCode === ENTER) {
+function keyPressed() { 
+    if(keyCode === ENTER) { // this creates a new snake if there is none and starts the game
         snake.start = true;
         if (snake.alive == false) {
             reset();
         }
 
     }
-    if (keyCode === 65) {
+    // this changes the direction of the snake when a specific key has been pressed (these can be changed)
+    if (keyCode === 65) { // 65 = a
       snake.setDir(-1, 0);
-    } else if (keyCode === 68) {
+    } else if (keyCode === 68) { // 68 = d
       snake.setDir(1, 0);
-    } else if (keyCode === 83) {
+    } else if (keyCode === 83) { // 83 = s
       snake.setDir(0, 1);
-    } else if (keyCode === 87) {
+    } else if (keyCode === 87) { // 87 = w
       snake.setDir(0, -1);
     }
   }
-function crossedLine(x,y,xoff,yoff,line) {
+function crossedLine(x,y,xoff,yoff,line) { // checks if the snake has crossed a border
     if (line.direction == 0) {
         return crossedLineOny(x,y,xoff,yoff,line);
     }
@@ -75,8 +76,8 @@ function crossedLine(x,y,xoff,yoff,line) {
         console.log("Error");
     }
 }
-function crossedLineOny(x,y,xoff,yoff,line) {
-    let linex = line.startX;
+function crossedLineOny(x,y,xoff,yoff,line) { // checks if the snake has crossed a border on the y axis
+    let linex = line.startX; 
     let parede = line.dentro;
     if(line.startY - line.endY > 0) {
         B = line.startY; 
@@ -99,7 +100,7 @@ function crossedLineOny(x,y,xoff,yoff,line) {
 
 
 }
-function crossedLineOnx(x,y,xoff,yoff,line) { 
+function crossedLineOnx(x,y,xoff,yoff,line) {  // checks if the snake has crossed a border on the x axis
     if(line.startX - line.endX > 0) {
         B = line.startX; 
         A = line.endX;
@@ -124,7 +125,7 @@ function crossedLineOnx(x,y,xoff,yoff,line) {
  */
 function mapMonitor(){
     push(); 
-    fill(0,255,0);
+    fill(255,209,0);
     noStroke();
     /**
      * Draws the first monitor rectangle.
@@ -168,25 +169,19 @@ function mapMonitor(){
      rect(1047,440,400,640);
     
     /**
-     * Draws the border of the first monitor.
+     * Draws the border (the empty space between the monitors) of monitors.
      */
-    borda1 = rect(879,66,60,400); // borda dos monitores
-    
-    /**
-     * Draws the border of the third monitor.
-     */
-    borda2 = rect(989,572,60,360);
+    borda1 = rect(879,66,60,400); // borda de cima 
+    borda2 = rect(989,572,60,360); // borda de baixo
     
     pop();
 }
-function isIn() {
+function isIn() { // generates the apple inside one of the monitors
     let monitors = [
         {x: 481, y: 0, width: 400, height: 640},
         {x: 881, y: 66, width: 640, height: 400},
-        {x: 879, y: 66, width: 60, height: 400},
-        {x: 989, y: 572, width: 60, height: 360},
-       
-        // add the other two monitors here
+        {x: 352, y: 572, width: 640, height: 360},
+        {x: 1047, y: 440, width: 400, height: 640},
     ];
 
     let monitor = random(monitors); // select a random monitor
--- snake.js
+++ snake.js
@@ -5,32 +5,33 @@ class Snake {
         this.speed = 2;
         this.alive = true;
         this.start = false
-        this.body = []; // array to store the positions of the bodythis
+        this.body = []; // array to store the positions of the body
         for(let i = 0; i < 5; i++) {
             this.body.push({x: this.x - i * 12, y: this.y}); // initialize the body positions
         }
         this.head = this.body[0]; 
     }
-    setDir(x, y) {
-        if(this.xspeed == -x || this.yspeed == -y) {
+    setDir(x, y) { // set the direction of the snake
+        if(this.xdir == -x || this.ydir == -y) {
             return;
         }
-        this.xspeed = x;
-        this.yspeed = y;
+        this.xdir = x;
+        this.ydir = y;
     }
-    show() {
+    show() { 
         push();
-        fill(128, 0, 128);
+
         for(let i = 0; i < this.body.length; i++) {
+            fill(colors[i], colors[i + 1], colors[i + 2])
             circle(this.body[i].x, this.body[i].y, 60); // draw the body at the stored positions
         }
-        fill(0);
-        let eyeRadius = 10;
-        let eyeOffset = 20;
-        let eyeX1 = this.body[0].x + this.xspeed * eyeOffset - this.yspeed * eyeOffset;
-        let eyeY1 = this.body[0].y + this.yspeed * eyeOffset + this.xspeed * eyeOffset;
-        let eyeX2 = this.body[0].x + this.xspeed * eyeOffset + this.yspeed * eyeOffset;
-        let eyeY2 = this.body[0].y + this.yspeed * eyeOffset - this.xspeed * eyeOffset;
+        fill(0);  // draw the eyes of the snake
+        let eyeRadius = 10; 
+        let eyeOffset = 20; 
+        let eyeX1 = this.body[0].x + this.xdir * eyeOffset - this.ydir * eyeOffset;
+        let eyeY1 = this.body[0].y + this.ydir * eyeOffset + this.xdir * eyeOffset;
+        let eyeX2 = this.body[0].x + this.xdir * eyeOffset + this.ydir * eyeOffset;
+        let eyeY2 = this.body[0].y + this.ydir * eyeOffset - this.xdir * eyeOffset;
         circle(eyeX1, eyeY1, eyeRadius);
         circle(eyeX2, eyeY2, eyeRadius);
         pop();
@@ -50,30 +51,30 @@ class Snake {
         this.body[0] = {x: this.x, y: this.y}; // the head of the snake is at the current position
     }
     step() 
-    {
+    {   // if the game has started (enter has been pressed while a snake is alive) the snake moves
         if (this.start == true){ 
-            this.speed = 2;
-            if(this.xspeed == undefined && this.yspeed == undefined){
-                this.xspeed = 1;
-                this.yspeed = 1;
+            // if the snake has not been given a direction it doesnt move and changes the 
+            if(this.xdir == undefined && this.ydir == undefined){ 
+                this.xdir = 1;
+                this.ydir = 1;
             }
             else {    
-            this.x += this.xspeed * this.speed;
-            this.y += this.yspeed * this.speed;
+            this.x += this.xdir * this.speed;
+            this.y += this.ydir * this.speed;
             }
         }
         else {
             this.speed = 0;
         }
     }
-    kill() {
+    kill() { // kill the snake
         this.start = false;
         this.body = [];
         this.x = 0;
         this.y = 0;
         this.alive = false;
     }
-    eat() {
+    eat() { // check if the snake has eaten the apple and if so creates a new apple and adds a body part
         let head = this.body[0];
         let d = dist(head.x, head.y, apple.loc.x, apple.loc.y);
         if(d < 35) {
